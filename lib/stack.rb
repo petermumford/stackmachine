fba26910ef51a675ca5a5938a9f1765b561fdf4d
Peter Mumford <peter@petermumford.com>
2014-05-14 11:29:15 +0100

    add each item in the array to the stack

--- lib/stack.rb
+++ lib/stack.rb
@@ -2,7 +2,13 @@ def stack_machine_emulator(s)
 	if s.nil? || s.empty?
 		puts "Sorry, you didn't enter a value"
 	else
+		stack = []
+
 		sArr = s.scan(/.{1}/)
+		sArr.each do |stack_item|
+			stack.push( stack_item )
+		end
+
 		puts "you entered #{sArr}"
 	end
 end

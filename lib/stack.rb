c2bf34b52e435336dc4c2cda9596ea8715c63bec
Peter Mumford <peter@petermumford.com>
2014-05-14 11:59:23 +0100

    check to make sure 2 consecutive symbols are the same

--- lib/stack.rb
+++ lib/stack.rb
@@ -3,15 +3,21 @@ def stack_machine_emulator(s)
 		puts "Sorry, you didn't enter a value"
 	else
 		stack = []
+		previous = ''
 
 		sArr = s.scan(/.{1}/)
 		sArr.each do |stack_item|
-			# check if stack_item is a digit
-			if stack_item =~ /^(\d)+$/
-				stack.push( stack_item.to_i )
+			if previous =~ /[+*]/ && previous.eql?(stack_item)
+				puts false
 			else
-				stack.push( stack.pop(2).inject( stack_item.to_sym ) )
+				# check if stack_item is a digit
+				if stack_item =~ /^(\d)+$/
+					stack.push( stack_item.to_i )
+				else
+					stack.push( stack.pop(2).inject( stack_item.to_sym ) )
+				end
 			end
+			previous = stack_item
 		end
 
 		puts "#{stack.last}"

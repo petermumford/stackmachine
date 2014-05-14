1b0b6d4d9230e112107bb977464fb6820de7bea3
Peter Mumford <peter@petermumford.com>
2014-05-14 12:44:47 +0100

    moved method into a class
    make sure the string has a minimum of 2 digits at the start

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,25 +1,32 @@
-def stack_machine_emulator(s)
-	if s.nil? || s.empty?
-		puts "Sorry, you didn't enter a value"
-	elsif s =~ /^[\d+*]*$/ && s !~ /[\+]{2,}|[\*]{2,}/
-		stack = []
+class StackMachine
+	attr_accessor :s
 
-		sArr = s.scan(/.{1}/)
-		sArr.each do |stack_item|
-			# check if stack_item is a digit
-			if stack_item =~ /^(\d)+$/
-				stack.push( stack_item.to_i )
-			else
-				stack.push( stack.pop(2).inject( stack_item.to_sym ) )
+	def initialize(s)
+		@s = s
+	end
+
+	def stack_machine_emulator
+		if @s.nil? || @s.empty?
+			puts "Sorry, you didn't enter a value"
+		elsif @s =~ /^[\d]{2}[\d+*]*$/ && @s !~ /[\+]{2,}|[\*]{2,}/
+			stack = []
+
+			sArr = @s.scan(/.{1}/)
+			sArr.each do |stack_item|
+				# check if stack_item is a digit
+				if stack_item =~ /^(\d)+$/
+					stack.push( stack_item.to_i )
+				else
+					stack.push( stack.pop(2).inject( stack_item.to_sym ) )
+				end
 			end
-		end
 
-		puts "#{stack.last}"
-	else
-		puts false
+			puts "#{stack.last}"
+		else
+			puts false
+		end
 	end
 end
 
 puts "Enter your stack:"
-s = gets.chomp
-stack_machine_emulator(s)
+StackMachine.new( gets.chomp ).stack_machine_emulator
